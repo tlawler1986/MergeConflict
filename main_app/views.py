97a92a8a0aed7490df7c32eb14607f74f8c59a11
gerardosdl <gerardo.sdl.os@gmail.com>
2025-07-11 15:42:03 -0400

    views.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,7 @@
+import requests
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView, ListView
 
 # Create your views here.
